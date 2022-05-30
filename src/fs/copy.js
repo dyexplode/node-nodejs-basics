eb0385db6e08ef6749630ceed4fbc5479a3d7658
Vladimir <admin@ttp.by>
2022-05-30 23:48:55 +0300

    feat: start fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,22 @@
+import { mkdir } from 'fs/promises';
+import { copyFile } from 'fs/promises';
+import { access } from 'fs/promises';
+import path from 'path';
+
 export const copy = async () => {
-    // Write your code here 
+    const distPath = path.join(path.resolve(), 'files_copy');
+    const srcPath = path.join(path.resolve(), 'filesa');
+    let isExistSrc;
+    let isExistDist;
+    try {
+        isExistSrc = !!await access(srcPath);
+    } catch {
+        isExistSrc = true;
+    }
+    try {}
+    // .then(() => console.log('1'))
+    // .catch(() => {throw new Error('FS operation failed')});
+    console.log('l', rez);
 };
+
+copy();
