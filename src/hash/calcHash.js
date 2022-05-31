413fc6397020014a460f2ad37ca55e264984dd37
Vladimir <admin@ttp.by>
2022-05-31 23:28:13 +0300

    feat: start hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,22 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { join, resolve } from 'path';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const r = () => {
+        return new Promise (resolv, reject)
+    }
+    const file = createReadStream(join(resolve(), 'files', 'fileToCalculateHashFor.txt'));
+    file.on('readable', () => {
+        const data = file.read();
+        if (data) hash.update(data);
+    })
+
+    file.on('end', () => {
+        console.log(hash.digest('hex'));
+        return hash.digest('hex');
+    })
 };
+
+calculateHash();
