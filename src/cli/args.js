3445075cb36c05a80db4cb09435f269e43cc5795
Vladimir <admin@ttp.by>
2022-05-31 16:35:11 +0300

    feat: complete cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,16 @@
+import process from 'process';
+
 export const parseArgs = () => {
-    // Write your code here 
+    const arrAtr = process.argv;
+    const getNext = arrAtr.shift.bind(arrAtr);
+    const result = [];
+    let arg; 
+    while(arg = getNext()) {
+        if (arg.match(/^--/)) {
+            result.push(`${arg.slice(2)} is ${getNext()}`);
+        }
+    }
+    console.log(result.join(', '));
 };
+
+parseArgs();
