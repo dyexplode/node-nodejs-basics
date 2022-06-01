cf5274ae66815e893401f1f1c68fabd66a111372
Vladimir <admin@ttp.by>
2022-06-01 15:53:57 +0300

    feat: start worker task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,18 @@
+import { Worker } from 'worker_threads';
+import path from 'path';
+
 export const performCalculations = async () => {
-    // Write your code here
+    const wPath = path.join(path.resolve(), 'worker.js');
+    const worker = new Worker(wPath, { workerData: 10 });
+    worker.on('exit', () => {
+        console.log('stoped');
+    })
+    // console.log(worker);
+
+    worker.on('message', (data) => {
+        console.log(data);
+    })
+
 };
+
+performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,15 @@
+import { parentPort } from 'worker_threads';
+
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
+    parentPort.on('message', () => {
+        const nth = 13;
+        parentPort.postMessage({ nth });
+    });
+    
     // This function sends result of nthFibonacci computations to main thread
 };
+
+sendResult();
