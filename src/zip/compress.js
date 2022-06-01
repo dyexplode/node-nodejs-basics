eea18940b14cde1127b036a6f7e721cbb8016936
Vladimir <admin@ttp.by>
2022-06-01 12:32:58 +0300

    feat: complete task hash, streams, zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,18 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { createGzip } from 'zlib';
+
 export const compress = async () => {
-    // Write your code here 
+    const zipper = createGzip();
+    const inpFile = createReadStream(path.join(path.resolve(), 'files', 'fileToCompress.txt'));
+    const outFile = createWriteStream(path.join(path.resolve(), 'files', 'archive.gz'));
+    pipeline(inpFile, zipper, outFile, (err) => {
+        if (err) {
+            console.log(err);
+            process.exitCode = 1;
+        }
+    })
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,18 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { createGunzip } from 'zlib';
+
 export const decompress = async () => {
-    // Write your code here 
+    const unzipper = createGunzip();
+    const inpFile = createReadStream(path.join(path.resolve(), 'files', 'archive.gz'));
+    const outFile = createWriteStream(path.join(path.resolve(), 'files', 'fileAfterDeCompress.txt'));
+    pipeline(inpFile, unzipper, outFile, (err) => {
+        if (err) {
+            console.log(err);
+            process.exitCode = 1;
+        }
+    })
 };
+
+decompress();
