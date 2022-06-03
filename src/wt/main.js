3bc8614acebd41e49cc1013d21ff21af69c7556a
Vladimir <admin@ttp.by>
2022-06-03 15:55:02 +0300

    feat: Run simple wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,17 +1,17 @@
 import { Worker } from 'worker_threads';
 import path from 'path';
+import { cpus } from 'os';
 
 export const performCalculations = async () => {
+    const cpuCount = await cpus().length;
+    console.log('cpu count: ', cpuCount);
     const wPath = path.join(path.resolve(), 'worker.js');
-    const worker = new Worker(wPath, { workerData: 10 });
-    worker.on('exit', () => {
-        console.log('stoped');
-    })
-    // console.log(worker);
-
-    worker.on('message', (data) => {
-        console.log(data);
-    })
+    for (let i = 0; i < cpuCount; i++){
+        const worker = new Worker(wPath, { workerData: 10 + i });
+        worker.on('message', (data) => {
+            console.log(data);
+        })
+    }
 
 };
 
