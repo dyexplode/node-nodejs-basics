366636508abb61e9dcadaa65aaee2932bb76c44f
Vladimir <admin@ttp.by>
2022-06-04 08:36:28 +0300

    fix: add change from second pc

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,23 @@
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import path from 'path';
+
 export const read = async () => {
-    // Write your code here 
+    const file = createReadStream(path.join(path.resolve(), 'files', 'fileToRead.txt'), { encoding: 'utf-8' });
+    
+    // Use pipe
+    file.pipe(stdout);
+    
+    // Without pipe
+    // file.on('readable', () => {
+    //     const data = file.read(); 
+    //     if (data) stdout.write(data);
+    //  })
+    
+    // Add return after cancel file
+    file.on('end', () => {
+        stdout.write('\n');
+    })
 };
+
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,11 @@
+import { stdin } from 'process';
+import { createWriteStream } from 'fs';
+import path from 'path';
+
 export const write = async () => {
-    // Write your code here 
+    const writer = createWriteStream(path.join(path.resolve(), 'files', 'fileToWrite.txt'), { encoding: 'utf-8' });
+    stdin.pipe(writer);
+    stdin.resume();
 };
+
+write();
