15697c65caa6c80edca4986caaa6b2ea3421c016
Vladimir <admin@ttp.by>
2022-06-05 15:15:37 +0300

    feat: Finished cp task. I hope this is true

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import child_process from 'child_process';
+import { stdin as input} from 'process';
 
 export const spawnChildProcess = async (args) => {
     // Get current directory
@@ -10,14 +11,20 @@ export const spawnChildProcess = async (args) => {
     [...args], { stdio: [0, 1, 2, 'ipc']});
 
    child.on('message', (data) => {
-       console.log('data', data);
+       console.log(`(MAIN) --> Recive from CHILD process: ${data}`);
    });
 
    child.on('close', (code) => {
-       console.log('code', code);
+       console.log('(MAIN) --> CHILD process exit with code:', code);
+       process.exit(0);
    })
 
-//    setTimeout(() => child.send('jjj'), 100);
+   input.on('data', (data) => {
+       console.log('(MAIN) --> Input to main process from stdin: ', data.toString());
+       child.send(data.toString());
+   })
+
+
 };
 
 spawnChildProcess([1, 2, 3, 4, 5, 6, 7, 8, 9]);
--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -1,15 +1,16 @@
 const argus = process.argv.slice(2);
 
-console.log(`Total number of arguments is ${argus.length}`);
-console.log(`Arguments: ${JSON.stringify(argus)}`);
+console.log(`(CHILD) --> Total number of arguments is ${argus.length}`);
+process.send(`(CHILD) --> Total number of arguments is ${argus.length}`);
+console.log(`(CHILD) --> Arguments: ${JSON.stringify(argus)}`);
+process.send(`(CHILD) --> Arguments: ${JSON.stringify(argus)}`);
 
 const echoInput = (chunk) => {
     const chunkStringified = chunk.toString();
     if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+    process.stdout.write(`(CHILD) --> Received from master process: ${chunk.toString()}\n`);
+    process.send(`(CHILD) --> Received from master process: ${chunk.toString()}\n`);
 };
 
-process.stdin.on('data', echoInput);
-// process.on('message', (dann) => {
-//     process.stdout.write(`Sent from Message IPC: ${dann}\n`);
-// })
+//process.stdin.on('data', echoInput);
+process.on('message', echoInput);
